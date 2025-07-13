8d1de89183f4edffef697df38dbb1e6247d5d132
Susu-spec <suleimansuwaibat@gmail.com>
2025-07-13 16:03:00 +0100

    feat(about):  adds assets and content for about section

--- src/pages/home/sections/about/index.tsx
+++ src/pages/home/sections/about/index.tsx
@@ -1,24 +1,51 @@
 export default function AboutSection() {
     return (
-        <section className="flex flex-col py-20 min-h-screen">
-            <div className="max-w-[20%] basis-[25%] ml-5">
-                
-                 <picture>
-                    <source media="(max-width: 480px)" srcSet="https://balance.michael-aust.com/media/pages/home/d09fd96a96-1743182714/index-about-placeholder-480x.webp" type="image/webp" />
-                    <source media="(max-width: 760px)" srcSet="https://balance.michael-aust.com/media/pages/home/d09fd96a96-1743182714/index-about-placeholder-760x.webp" type="image/webp" />
-                    <source media="(max-width: 1200px)" srcSet="https://balance.michael-aust.com/media/pages/home/d09fd96a96-1743182714/index-about-placeholder-1200x.webp" type="image/webp" />
-                    <source media="(max-width: 1920px)" srcSet="https://balance.michael-aust.com/media/pages/home/d09fd96a96-1743182714/index-about-placeholder-1920x.webp" type="image/webp" />
-                    <source media="(min-width: 1921px)" srcSet="https://balance.michael-aust.com/media/pages/home/d09fd96a96-1743182714/index-about-placeholder-2560x.webp" type="image/webp" />
-                    <img src="https://balance.michael-aust.com/media/pages/home/d09fd96a96-1743182714/index-about-placeholder-2560x.webp" />
-                </picture>
+        <section className="flex flex-col gap-[5rem] py-20 min-h-screen max-h-fit lg:px-8 relative">
+            <div className="relative h-full">
+                 <div className="max-w-[20%] basis-[25%] ml-24"> 
+                    <picture>
+                        <source media="(max-width: 480px)" srcSet="https://balance.michael-aust.com/media/pages/home/d09fd96a96-1743182714/index-about-placeholder-480x.webp" type="image/webp" />
+                        <source media="(max-width: 760px)" srcSet="https://balance.michael-aust.com/media/pages/home/d09fd96a96-1743182714/index-about-placeholder-760x.webp" type="image/webp" />
+                        <source media="(max-width: 1200px)" srcSet="https://balance.michael-aust.com/media/pages/home/d09fd96a96-1743182714/index-about-placeholder-1200x.webp" type="image/webp" />
+                        <source media="(max-width: 1920px)" srcSet="https://balance.michael-aust.com/media/pages/home/d09fd96a96-1743182714/index-about-placeholder-1920x.webp" type="image/webp" />
+                        <source media="(min-width: 1921px)" srcSet="https://balance.michael-aust.com/media/pages/home/d09fd96a96-1743182714/index-about-placeholder-2560x.webp" type="image/webp" />
+                        <img src="https://balance.michael-aust.com/media/pages/home/d09fd96a96-1743182714/index-about-placeholder-2560x.webp" />
+                    </picture>
+                </div>
+                <h1 className="font-inter-tight font-bold text-black text-[3.5rem] leading-[3rem]">
+                    <span className="block pb-2"> <span className="pl-[24.72rem]">I help brands</span> craft meaningful</span>
+                    <span className="block pb-2">stories and compelling visuals that deeply</span>
+                    <span className="block pb-2">resonate with their audience, foster strong</span>
+                    <span className="block pb-2">connections, build lasting loyalty, and drive</span>
+                    <span className="block">sustainable long-term growth.</span>
+                </h1>
             </div>
-            <h1 className="font-inter-tight font-bold text-black text-[3.5rem]">
-                I help brands craft meaningful
-                stories and compelling visuals that deeply
-                resonate with their audience, foster strong
-                connections, build lasting loyalty, and drive
-                sustainable long-term growth.
-            </h1>
+            <div className="w-full flex justify-end">
+                <div className="max-w-[50%]">
+                    <hr className="w-full text-gray-300 mb-6" />
+                    <div className="w-full flex">
+                        <div className="max-w-[50%] flex flex-col gap-3 px-[.94rem]">
+                            <p className="font-bold text-black text-sm">From A to Z</p>
+                            <p className="font-medium text-sm text-gray-400">
+                                I manage your entire branding process, from
+                                concept to execution. Whether it's logo design,
+                                messaging, or strategy, I ensure everything
+                                aligns for a cohesive and impactful brand.
+                            </p>
+                            <a href="/about" className="about-button" title="About | Suwayba">More about me</a>
+                        </div>
+                        <div className="max-w-[50%] flex flex-col gap-3 px-[.94rem] ">
+                            <p className="font-bold text-black text-sm">Solo or Team</p>
+                            <p className="font-medium text-sm text-gray-400">
+                                I work both independently and with a trusted
+                                team, adapting to your project’s needs to
+                                deliver the best results, whether it's a solo
+                                vision or a collaborative effort.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
--- src/pages/home/sections/hero/index.tsx
+++ src/pages/home/sections/hero/index.tsx
@@ -1,6 +1,7 @@
+import HeroBg from "@/assets/hero-bg02.png"
 export default function HeroSection() {
     return (
-         <section className="pl-4 pb-[11.25rem] h-screen relative">
+         <section className="pl-4 h-screen relative overflow-hidden">
             <ul className="flex flex-col gap-0 text-start absolute ml-[50%] px-6 py-4 z-40">
                 <a href="/" className="nav-link">Home</a>
                 <a href="/about" className="nav-link">About</a>
@@ -10,35 +11,35 @@ export default function HeroSection() {
                 </a>
                 <a href="/contact" className="nav-link">Contact</a>
             </ul>
-            <p className="capitalize !text-[11.25rem] !font-inter-tight font-bold absolute bottom-6 z-5 leading-[8.125rem]">
-              SUWAYBA
-            </p>    
-            <div className="h-full grid grid-cols-2 relative">
-                <div className="relative flex flex-col justify-end ">
-                <div className="max-w-[50%] leading-[1.125rem] absolute bottom-[10.25rem]">
-                    <p className="font-medium text-xs">
-                        Frontend Engineer
-                    </p>
-                    <p className="text-gray-400 text-xs">
-                        I build interactive user interfaces that 
-                        connect users to the backend logic of applications. 
-                        My work ensures that digital products are usable, 
-                        responsive, and visually appealing across devices.
-                    </p>
-                </div>
+            
+            <div className="grid grid-cols-2 h-full">
+                <div className="flex flex-col relative justify-end">
+                    <div className="max-w-[50%] leading-[1.125rem]">
+                        <p className="font-medium text-xs">
+                            Frontend Engineer
+                        </p>
+                        <p className="text-gray-400 text-xs">
+                            I build interactive user interfaces that 
+                            connect users to the backend logic of applications. 
+                            My work ensures that digital products are usable, 
+                            responsive, and visually appealing across devices.
+                        </p>
+                        <p className="capitalize mt-3 text-gray-900 !text-[11.25rem] !font-inter-tight font-bold z-5 leading-[8.125rem]">
+                        SUWAYBA
+                        </p>   
+                    </div>
                 </div>
                 
-                <div className="w-full h-full">
+                <div className="w-full h-full max-h-screen">
                     <picture>
-                        <source media="(max-width: 480px)" srcSet="https://balance.michael-aust.com/media/pages/home/9f06b010e0-1743182714/header-img-placeholder-480x840-crop.webp" />
+                        {/* <source media="(max-width: 480px)" srcSet="https://balance.michael-aust.com/media/pages/home/9f06b010e0-1743182714/header-img-placeholder-480x840-crop.webp" />
                         <source media="(max-width: 760px)" srcSet="https://balance.michael-aust.com/media/pages/home/9f06b010e0-1743182714/header-img-placeholder-760x.webp"/>
                         <source media="(max-width: 1200px)" srcSet="https://balance.michael-aust.com/media/pages/home/9f06b010e0-1743182714/header-img-placeholder-1200x.webp"/>
                         <source media="(max-width: 1920px)" srcSet="https://balance.michael-aust.com/media/pages/home/9f06b010e0-1743182714/header-img-placeholder-1920x.webp"/>
-                        <source media="(min-width: 1921px)" srcSet="https://balance.michael-aust.com/media/pages/home/9f06b010e0-1743182714/header-img-placeholder-2560x.webp"/>
-                        <img className="h-full" 
+                        <source media="(min-width: 1921px)" srcSet="https://balance.michael-aust.com/media/pages/home/9f06b010e0-1743182714/header-img-placeholder-2560x.webp"/> */}
+                        <img className="w-full" 
                             alt="Dark skinned man in white suit looking to the left in a bright studio next to a table with flowers on it." 
-                            src="https://balance.michael-aust.com/media/pages/home/9f06b010e0-1743182714/header-img-placeholder-2560x.webp" 
-                            data-src="https://balance.michael-aust.com/media/pages/home/9f06b010e0-1743182714/header-img-placeholder-2560x.webp" 
+                            src={HeroBg} 
                         />
                         
                     </picture>
