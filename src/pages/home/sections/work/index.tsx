4600f5be700665f719de3e1bb2a3e6afd001b27e
Susu-spec <suleimansuwaibat@gmail.com>
2025-07-16 22:25:53 +0100

    feat(work): adds base content for project components

--- src/pages/home/sections/work/index.tsx
+++ src/pages/home/sections/work/index.tsx
@@ -1,116 +1,64 @@
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
-import { useRef } from "react";
-import synesthesiaImage from "@/assets/synesthesia.png";
-import astralPlane from "@/assets/astral-plane.png";
-import spaceStars from "@/assets/space-stars.png";
-import dragons from "@/assets/dragons.png";
-
-gsap.registerPlugin(ScrollTrigger);
+import synesthesiaImage from "@/assets/blue.png";
+import astralPlane from "@/assets/r2d2.png";
+import spaceStars from "@/assets/space-invaders.gif";
+import dragons from "@/assets/castle.png";
 
 
-export default function WorkSection() {
-   const sectionsRef = useRef<HTMLDivElement[]>([]);
-useGSAP(() => {
-    const sections = sectionsRef.current;
-   
-    const setupVerticalScroll = () => {
-        sections.forEach((section, index) => {
-            if (index === 0) return; // Skip animation for first section
-            
-            gsap
-                .timeline({
-                    scrollTrigger: {
-                        trigger: section,
-                        start: 'top bottom',
-                        end: 'bottom top',
-                        scrub: 3.5,
-                    }
-                })
-                .fromTo(section, {
-                    y: '100%', // Start from below
-                }, {
-                    y: '0%',   // Move to normal position
-                    ease: 'none',
-                    duration: 4,
-                }, '<');
-        })
-    }
-    setupVerticalScroll();
-
-    return () => {
-        ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-    };
-}, []);
-
-const addToRefs = (el: HTMLDivElement | null) => {
-    if (el && !sectionsRef.current.includes(el)) {
-        sectionsRef.current.push(el);
-    }
-};
-
-const sectionComponents = [
-    <Synesthesia />,
-    <SpaceInvaders />,
-    <DungeonsAndDragons />,
-    <R2D2 />
-]
-
-return (
-    <section className="sticky top-h-screen overflow-hidden">
-        <div className="relative h-full">
-            {sectionComponents.map((SectionComponent, index) => (
-                <div
-                    key={index}
-                    ref={addToRefs}
-                    className={`h-full sticky top-0 cursor-default ${index > 0 ? 'z-10' : ''}`}
-                    style={{ zIndex: index + 1 }}
-                >
-                    {SectionComponent}
+export const Synesthesia = () => {
+    return (
+        
+            <picture className="w-full h-screen relative">
+                <img
+                    // alt="Top half of woman's head who has dark hair in front of plain background." 
+                    src={synesthesiaImage} className="w-full h-screen object-fill" />
+               
+                <div className="z-20 absolute bottom-2 right-2 text-[10px] tracking-wider text-zinc-900">
+                    berlin · via  
+                    <a href=" https://www.instagram.com/p/C5A7xNTshk5/?igsh=ZzR4dWJ1bnc0cHJo&img_index=3" 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                        className="underline"
+                    >
+                         duc trinh
+                    </a>
                 </div>
-            ))}
-        </div>
-    </section>
-);
-}
-
-const Synesthesia = () => {
-    return (
-        <picture className="absolute w-full h-full top-0 -z-20">
-            <img 
-                // alt="Top half of woman's head who has dark hair in front of plain background." 
-                src={synesthesiaImage} className="w-full"/>
-        </picture>
+            </picture>
+        
     )
 }
 
-const SpaceInvaders = () => {
+export const SpaceInvaders = () => {
     return (
-        <picture className="absolute w-full h-full top-0 -z-20">
-            <img 
-                // alt="Top half of woman's head who has dark hair in front of plain background." 
-                src={spaceStars} className="w-full"/>
-        </picture>
+        
+            <picture className="w-full h-screen relative">
+                <img 
+                    // alt="Top half of woman's head who has dark hair in front of plain background." 
+                    src={spaceStars} className="w-full h-screen object-fill" />
+            </picture>
+        
     )
 }
 
-const DungeonsAndDragons = () => {
+export const DungeonsAndDragons = () => {
     return (
-        <picture className="absolute w-full h-full top-0 -z-20">
-            <img 
-                // alt="Top half of woman's head who has dark hair in front of plain background." 
-                src={dragons} className="w-full"/>
-        </picture>
+        
+            <picture className="w-full h-screen relative">
+                <img 
+                    // alt="Top half of woman's head who has dark hair in front of plain background." 
+                    src={dragons} className="w-full h-screen object-cover" />
+            </picture>
+        
     )
 }
 
-const R2D2 = () => {
+export const R2D2 = () => {
     return (
-        <picture className="absolute w-full h-full top-0 -z-20">
-            <img 
-                // alt="Top half of woman's head who has dark hair in front of plain background." 
-                src={astralPlane} className="w-full"/>
-        </picture>
+        
+            <picture className="w-full h-screen relative">
+                <img 
+                    // alt="Top half of woman's head who has dark hair in front of plain background." 
+                    src={astralPlane} className="w-full h-screen object-cover" />
+            </picture>
+        
     )
 }
