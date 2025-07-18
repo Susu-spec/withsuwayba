bd174822f787d9bbb21d7f78499cc82402c12c4d
Susu-spec <suleimansuwaibat@gmail.com>
2025-07-18 20:03:44 +0100

    feat(footer): adds `scrollToTop` function and design adjustments

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,11 @@
 export default function Footer() {
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
+
     return (
         <footer className="h-screen relative bg-[#FCFCFC] flex flex-col justify-between">
             <div className="flex justify-between">
@@ -13,7 +20,7 @@ export default function Footer() {
                         <a href="/contact" className="nav-link !text-[5.3rem]">Contact</a>
                     </ul>
                 </div>
-                <div className="flex flex-col justify-end w-full">
+                <div className="flex flex-col justify-end w-full max-w-[20%] items-end px-[.94rem]">
                     <div className="flex flex-col w-full">
                         <div className="flex items-center gap-[.94rem] flex-wrap">
                             <a href="https://instagram.com/" target="_blank" title="Visit on Instagram">
@@ -33,27 +40,37 @@ export default function Footer() {
                             </a>
                         </div>
                         <div className="flex flex-col gap-[.94rem]">
-                        <p className="flex flex-col">
-                                <span className="text-gray-400">(Inquiries)</span>
-                                <span>suwayba@studio.com</span>
-                        </p>
-                        <p className="flex flex-col">
-                                <span className="text-gray-400">(Phone)</span>
-                                <span>+491234 56789</span>
-                        </p>
+                            <p className="flex flex-col text-sm">
+                                    <span className="text-gray-400">(Inquiries)</span>
+                                    <a className="underline">suwayba@studio.com</a>
+                            </p>
+                            <p className="flex flex-col text-sm">
+                                    <span className="text-gray-400">(Phone)</span>
+                                    <span className="underline">+491234 56789</span>
+                            </p>
                         </div>
                     </div>
 
                 </div>
             </div>
-            <div className="w-full">
-                <hr className="w-full text-gray-300 mb-6" />
-                <div className="my-[1.98rem] flex justify-between">
+            <div className="w-full px-4 absolute bottom-18">
+                <hr className="w-full text-gray-400 mb-6" />
+                <div className="mt-[1.5rem] flex justify-between">
                     <ul className="navi-bottom text-sm">
-                        <a className="text-gray-300" href="/" title="Imprint">Imprint</a>
-                        <a className="text-gray-300" href="/" title="Imprint">Privacy Policy</a>
+                        <a className="text-gray-400 font-medium" href="/" title="Imprint">Imprint</a>
+                        <a className="text-gray-400 font-medium" href="/" title="Imprint">Privacy Policy</a>
                     </ul>
+                    <button 
+                        onClick={scrollToTop} 
+                        className="text-sm text-gray-400 cursor-pointer"
+                    >
+                        &uarr;
+                        Back to Top
+                    </button>
                 </div>
+            </div>
+            <div className="bg-black w-full text-center absolute bottom-0">
+                <p className="text-white text-xs py-4">Design Credits - <a href="/">Michael Aust</a></p>
             </div>
         </footer>
     )
