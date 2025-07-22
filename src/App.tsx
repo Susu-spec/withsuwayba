ba236f65f0c0b14d9aa80511ea548f70a27737b9
Susu-spec <suleimansuwaibat@gmail.com>
2025-07-22 19:18:47 +0100

    fix: deployment error on `app.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./pages/home";
-import About from "./pages/about";
-import Work from "./pages/work";
-import Contact from "./pages/contact";
+// import About from "./pages/about";
+// import Work from "./pages/work";
+// import Contact from "./pages/contact";
 
 
 function App() {
@@ -10,9 +10,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<About />} />
+        {/* <Route path="/about" element={<About />} />
         <Route path="/work" element={<Work />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/contact" element={<Contact />} /> */}
       </Routes>
     </BrowserRouter>
   )
