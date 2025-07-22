3b5356d98df6c8c4f92b1e76cfaf9f5c31cec018
Susu-spec <suleimansuwaibat@gmail.com>
2025-07-22 19:02:02 +0100

    feat: remove unused folders

--- src/pages/work/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Work() {
-    return (
-        <section className="section bg-muted-white">
-            <p className="text-muted-white">work</p>
-        </section>
-    );
-}
